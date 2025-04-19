fdf85b28ceb689ab2a6e0bfe065f4a03e19d45ac
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-19 14:38:42 +0800

    systems/apus/disk: add main disk

--- systems/apus/disk.nix
+++ systems/apus/disk.nix
@@ -1,2 +1,34 @@
 {
+  disko.devices.disk.main = {
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          type = "EF00";
+          size = "1G";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        root = {
+          type = "8304";
+          size = "100%";
+          content = {
+            name = "root";
+            type = "luks";
+            passwordFile = "/tmp/disk.key";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
 }
