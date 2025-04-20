e4bbf4ac988b1c8cca1f8a3c7234ef5a063973da
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-20 15:42:48 +0800

    systems/apus/packages: add steam

--- systems/apus/packages.nix
+++ systems/apus/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   hardware.enableRedistributableFirmware = true;
 
@@ -10,8 +10,17 @@
 
   networking.networkmanager.enable = true;
 
+  programs.steam.enable = true;
+
   fonts.packages = with pkgs; [
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
   ];
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "steam"
+      "steam-unwrapped"
+    ];
 }
