c585c5c6eb24d0339ea3b71e3f0d11d54b42e482
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-21 18:55:56 +0800

    systems/apus: enable fcitx5 with wayland

--- systems/apus/desktop.nix
+++ systems/apus/desktop.nix
@@ -1,4 +1,14 @@
+{ pkgs, ... }:
 {
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = [ pkgs.fcitx5-rime ];
+    };
+  };
 }
