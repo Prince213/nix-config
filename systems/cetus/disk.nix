be65d8a301ce6f299e4d06fef1000c37466c56b7
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-28 15:56:38 +0800

    systems/cetus/disk: add main disk

--- systems/cetus/disk.nix
+++ systems/cetus/disk.nix
@@ -1,2 +1,29 @@
 {
+  disko.devices.disk.main = {
+    device = "/dev/sda";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          type = "EF00";
+          size = "1G";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        root = {
+          type = "8304";
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
 }
