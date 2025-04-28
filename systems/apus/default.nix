852cf7a088a2f47bf18d8293ffee422d192adaf3
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-28 15:42:29 +0800

    systems/apus: refactor

--- systems/apus/default.nix
+++ systems/apus/default.nix
@@ -1,19 +1,14 @@
 {
-  nixpkgs.hostPlatform = "x86_64-linux";
-
   system.stateVersion = "25.05";
-
   networking.hostName = "apus";
+  nixpkgs.hostPlatform = "x86_64-linux";
+  time.timeZone = "Asia/Shanghai";
 
   i18n.supportedLocales = [
     "C.UTF-8/UTF-8"
     "en_US.UTF-8/UTF-8"
     "zh_CN.UTF-8/UTF-8"
   ];
-
-  time.timeZone = "Asia/Shanghai";
-
-  environment.sessionVariables.NIXOS_OZONE_WL = 1;
 
   imports = [
     ./boot.nix
--- systems/apus/desktop.nix
+++ systems/apus/desktop.nix
@@ -3,6 +3,8 @@
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
 
+  environment.sessionVariables.NIXOS_OZONE_WL = 1;
+
   fonts.fontconfig = {
     defaultFonts = {
       serif = [ "Noto Serif CJK SC" ];
