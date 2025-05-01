df8391fa45c93bcfca8978b48a2b3ae5aaabe3d5
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-01 22:48:34 +0800

    flake: add nix-topology

--- flake.nix
+++ flake.nix
@@ -26,6 +26,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-topology = {
+      url = "github:oddlama/nix-topology";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -57,6 +61,7 @@
       home-manager,
       neovim-nightly,
       nix-index-database,
+      nix-topology,
       nixvim,
       plasma-manager,
       treefmt-nix,
