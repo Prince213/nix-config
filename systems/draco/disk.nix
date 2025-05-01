7d4d58974aac57ce800c75668dce0ec2d2755f4e
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-01 15:58:18 +0800

    systems/draco/disk: add main disk

--- systems/draco/disk.nix
+++ systems/draco/disk.nix
@@ -1,2 +1,23 @@
 {
+  disko.devices.disk.main = {
+    device = "/dev/sda";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          type = "EF02";
+          size = "1M";
+        };
+        root = {
+          type = "8304";
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
 }
