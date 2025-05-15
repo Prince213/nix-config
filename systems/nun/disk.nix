908abd7fedf71b861303e52830b1032cc1b2c2b1
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-15 21:56:05 +0800

    systems/nun/disk: add main disk

--- systems/nun/disk.nix
+++ systems/nun/disk.nix
@@ -1,4 +1,24 @@
 {
-  disko = {
+  disko.devices.disk.main = {
+    device = "/dev/vda";
+    imageSize = "10G";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          type = "EF02";
+          size = "1M";
+        };
+        root = {
+          type = "8304";
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
   };
 }
