221fcfd6808a7fdc4eec5e4d8ec2ecfdb414d401
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-18 14:47:16 +0800

    homes/apus/prince213/desktop: add bottom panel

--- homes/apus/prince213/desktop.nix
+++ homes/apus/prince213/desktop.nix
@@ -17,6 +17,20 @@
         };
       };
     };
+    panels = [
+      {
+        floating = true;
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.pager"
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+          "org.kde.plasma.showdesktop"
+        ];
+      }
+    ];
   };
 
   xdg.mimeApps = {
