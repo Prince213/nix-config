a85c14431655b3298848cd85d1c94516c7ac1c12
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-18 11:08:22 +0800

    homes/apus/prince213/chrome: fix ime

--- homes/apus/prince213/chrome.nix
+++ homes/apus/prince213/chrome.nix
@@ -3,5 +3,10 @@
   programs.chromium = {
     enable = true;
     package = pkgs.google-chrome;
+    # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
+    # chrome://flags/#wayland-text-input-v3
+    # commandLineArgs = [
+    #   "--wayland-text-input-version=1"
+    # ];
   };
 }
