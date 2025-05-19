f125e1c4a1c390850464b8e45152d54deb9763e2
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-19 20:49:35 +0800

    homes/apus/prince213/starship: set prompt format

--- homes/apus/prince213/starship.nix
+++ homes/apus/prince213/starship.nix
@@ -2,6 +2,9 @@
   programs.starship = {
     enable = true;
     settings = {
+      format = ''
+        $directory$git_branch$git_commit$git_metrics$time$line_break$jobs$character
+      '';
       git_metrics = {
         disabled = false;
       };
