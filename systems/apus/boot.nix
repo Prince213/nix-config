84661a203765269aa4d122a81b3db32d8ab8d444
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-21 21:40:55 +0800

    systems/apus/boot: use limine

--- systems/apus/boot.nix
+++ systems/apus/boot.nix
@@ -1,5 +1,13 @@
 {
   hardware.cpu.intel.updateMicrocode = true;
 
+  boot.loader = {
+    grub.enable = false;
+    limine = {
+      enable = true;
+    };
+    systemd-boot.enable = false;
+  };
+
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 }
