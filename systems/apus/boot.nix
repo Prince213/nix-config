33efd9f3d2cee1458834443a64fc9bf8af887272
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-25 15:37:53 +0800

    systems/apus/boot: use systemd-boot

--- systems/apus/boot.nix
+++ systems/apus/boot.nix
@@ -1,13 +1,5 @@
 {
   hardware.cpu.intel.updateMicrocode = true;
 
-  boot.loader = {
-    grub.enable = false;
-    limine = {
-      enable = true;
-    };
-    systemd-boot.enable = false;
-  };
-
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 }
