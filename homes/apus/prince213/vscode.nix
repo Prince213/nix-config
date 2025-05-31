528e79422d1dbbb25d3dbe99b03515cba92f42d8
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-31 14:34:24 +0800

    homes/apus/prince213/vscode: make extensions mutable

--- homes/apus/prince213/vscode.nix
+++ homes/apus/prince213/vscode.nix
@@ -3,7 +3,7 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    mutableExtensionsDir = false;
+    mutableExtensionsDir = true;
     profiles.default = {
       enableExtensionUpdateCheck = false;
       enableUpdateCheck = false;
